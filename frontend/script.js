b08562e7f34ae4fb13a883bba90d9aa7547ba073
Louis <louischin19960802@yahoo.com>
2025-11-01 00:36:31 +0800

    Refactored code related to handling edit and delete item button at the frontend, better encapsulation, code is cleaner, easier to read and maintain. Added functionality to cancel edit. Decorated frontend a bit. Pressing enter now submits form correctly. Built the backend project and generated the updated .jar file.

--- frontend/script.js
+++ frontend/script.js
@@ -6,7 +6,8 @@ document.getElementById("amount-input").addEventListener("input", function(){
     this.value = Math.max(0,this.value);
 });
 
-async function addItem(){
+async function addItem(event){
+    event.preventDefault();
     const form = document.getElementById("add-item-form");
     const formData = new FormData(form);
     const data = Object.fromEntries(formData.entries());
@@ -43,7 +44,8 @@ function printAddItemResponseMessage(response, responseData){
     }
 }
 
-async function getItem(){
+async function getItem(event){
+    event.preventDefault();
     const form = document.getElementById("get-item-form");
     const formData = new FormData(form);
     const data = Object.fromEntries(formData.entries());
@@ -72,8 +74,6 @@ function displayItemTable(responseData){
 
 function addRow(table, item, fieldOrder) {
     const rowObject = new RowObject(item, fieldOrder);
-    rowObject.editButton.addEventListener("click", editItem);
-    rowObject.deleteButton.addEventListener("click", deleteItem);
     table.appendChild(rowObject.row);
 }
 
@@ -83,161 +83,6 @@ function printGetItemResponseMessage(response, responseData){
     switch(response.status){
         case 200:
             textObj.innerText = responseData.length + " search results found.";
-            break;
-        case 400:
-        case 404:
-        case 500:
-            textObj.innerText = responseData.errorMessage;
-            break;
-        default:
-            textObj.innerText = "Unexpected error occurred.";
-    }
-}
-
-async function deleteItem(event){
-    const confirmed = confirm("Are you sure you want to delete this item?");
-    if(!confirmed){
-        return;
-    }
-    const button = event.currentTarget;
-    const rowObject = button.rowObject;
-    const item = rowObject.item;
-
-    const url = "http://localhost:8080/item/" + item.id;
-    console.log("Deleting item with ID: " + item.id);
-    const response = await fetch(url, {
-        method: "DELETE",
-    })
-    let responseData = null; //204 has no body, may cause error when calling response.json()
-    if(response.status !== 204){
-        responseData = await response.json();
-    }
-    printDeleteItemResponseMessage(response, responseData);
-    if(response.status === 204){
-        const table = document.querySelector("#display-item-table tbody");
-        table.removeChild(rowObject.row);
-    }
-}
-
-function printDeleteItemResponseMessage(response, responseData){
-    const textObj = document.getElementById("delete-edit-item-text");
-    textObj.innerText = "";
-    switch(response.status){
-        case 204:
-            textObj.innerText = "Item deleted successfully!";
-            break;
-        case 400:
-        case 404:
-        case 500:
-            textObj.innerText = responseData.errorMessage;
-            break;
-        default:
-            textObj.innerText = "Unexpected error occurred.";
-    }
-}
-
-async function editItem(event){
-    const button = event.currentTarget;
-    const rowObject = button.rowObject;
-    const row = rowObject.row;
-    let item = rowObject.item;
-    const cells = row.querySelectorAll("td");
-    if(button.mode === "edit"){
-        button.mode = "submit";
-        button.innerHTML = '<i class="bi bi-check2-all"></i>';
-        cells.forEach(cell => addInputToCell(cell, item));
-    } else if (button.mode === "submit"){
-        button.mode = "edit";
-        button.innerHTML = '<i class="bi bi-pencil-fill"></i>';
-        if(!isInputChanged(cells, item)){
-            document.getElementById("delete-edit-item-text").innerText = "No changes detected.";
-        } else {
-            const newItem = generateItemFromInput(item.id, cells);
-            const url = "http://localhost:8080/item/" + newItem.id;
-            const jsonData = JSON.stringify(newItem);
-            console.log("Updating item with ID: " + newItem.id + " with data: " + jsonData);
-            const response = await fetch(url, {
-                method: "PUT",
-                headers: {"Content-Type": "application/json"},
-                body: jsonData
-            })
-            let responseData = null; //204 has no body, may cause error when calling response.json()
-            if(response.status !== 204){
-                responseData = await response.json();
-            } else {
-                item = Object.assign(item, newItem); 
-                //update local item only if update successful so that restoreCellDisplay works correctly
-            }
-            printEditItemResponseMessage(response, responseData);
-        }
-        cells.forEach(cell => restoreCellDisplay(cell, item));
-    }
-}
-
-function addInputToCell(cell, item){
-    if(cell.type === "field"){
-        cell.textContent = "";
-        const value = item[cell.name];
-        const input = document.createElement("input");
-        input.type = "text";
-        input.value = value;
-        input.style.width = "100%";
-        if(cell.name === "amount"){
-            input.type = "number";
-            input.min = "0";
-            input.addEventListener("input",function(){
-                this.value = Math.max(0,this.value);
-            });
-        }
-        cell.appendChild(input);
-    }
-}
-
-function isInputChanged(cells, item){
-    let changed = false;
-    for(const cell of cells){
-        if(cell.type === "field"){
-            const input = cell.querySelector("input");
-            let value = input.value.trim();
-            if(cell.name === "amount"){
-                value = Number(value);
-            }
-            if(value !== item[cell.name]){
-                changed = true;
-                break;
-            }
-        }
-    }
-    return changed;
-}
-
-function restoreCellDisplay(cell, item){
-    if(cell.type === "field"){
-        cell.textContent = item[cell.name];
-    }
-}
-
-function generateItemFromInput(id, cells){
-    const item = {};
-    item.id = id;
-    for(const cell of cells){
-        if(cell.type === "field"){
-            item[cell.name] = cell.querySelector("input").value.trim();
-            if(cell.name === "amount"){
-                item[cell.name] = Number(item[cell.name]);
-            }
-        }
-    }
-    console.log("Generated item = " + JSON.stringify(item));
-    return item;
-}
-
-function printEditItemResponseMessage(response, responseData){
-    const textObj = document.getElementById("delete-edit-item-text");
-    textObj.innerText = "";
-    switch(response.status){
-        case 204:
-            textObj.innerText = "Item updated successfully!";
             break;
         case 400:
         case 404:
